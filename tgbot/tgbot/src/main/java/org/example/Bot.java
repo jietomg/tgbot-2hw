e5c986147b2193d668b73a19750f8e36c865ab4f
JieTo <124500694+jietomg@users.noreply.github.com>
2023-10-14 13:37:46 +0300

    Add files via upload

---
+++ tgbot/tgbot/src/main/java/org/example/Bot.java
@@ -0,0 +1,133 @@
+package org.example;
+
+import org.example.func.FilterOperations;
+import org.example.utils.ImageUtils;
+import org.example.utils.PhotoMessageUtils;
+import org.example.utils.RgbMaster;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.GetFile;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.PhotoSize;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        String chatId = message.getChatId().toString();
+        try {
+            ArrayList<String> photoPaths = new ArrayList<>(PhotoMessageUtils.savePhotos(getFilesByMessage(message), getBotToken()));
+            for (String path : photoPaths) {
+                processingImage(path);
+                execute(preparePhotoMessage(path, chatId));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    private SendPhoto preparePhotoMessage(String localPath, String chatId) {
+        SendPhoto sendPhoto = new SendPhoto();
+        sendPhoto.setReplyMarkup(getKeyboard(FilterOperations.class));
+        sendPhoto.setChatId(chatId);
+        InputFile newFile = new InputFile();
+        newFile.setMedia(new File(localPath));
+        sendPhoto.setPhoto(newFile);
+        return sendPhoto;
+    }
+
+
+    private ReplyKeyboardMarkup getKeyboard(Class someClass) {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
+        Method[] methods = someClass.getMethods();
+        int columnCount = 3;
+        int rowsCount = methods.length / columnCount + ((methods.length %
+                columnCount == 0) ? 0 : 1);
+        for (int rowIndex = 0; rowIndex < rowsCount; rowIndex++) {
+            KeyboardRow row = new KeyboardRow();
+            for (int columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+                int index = rowIndex * columnCount + columnIndex;
+                if (index >= methods.length) continue;
+                Method method = methods[rowIndex * columnCount + columnIndex];
+                KeyboardButton keyboardButton = new
+                        KeyboardButton(method.getName());
+                row.add(keyboardButton);
+            }
+            keyboardRows.add(row);
+        }
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+        return replyKeyboardMarkup;
+    }
+
+    private ArrayList<org.telegram.telegrambots.meta.api.objects.File>
+    getFilesByMessage(Message message) {
+        List<PhotoSize> photoSizes = message.getPhoto();
+        ArrayList<org.telegram.telegrambots.meta.api.objects.File> files =
+                new ArrayList<>();
+        for (PhotoSize photoSize : photoSizes) {
+            final String fileId = photoSize.getFileId();
+            try {
+                files.add(sendApiMethod(new GetFile(fileId)));
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+        return files;
+    }
+
+
+
+    public void processingImage(String fileName) throws Exception {
+        final BufferedImage image = ImageUtils.getImage(fileName);
+        final RgbMaster rgbMaster = new RgbMaster(image);
+        rgbMaster.changeImage(FilterOperations::grayScale);
+        ImageUtils.saveImage(rgbMaster.getImage(), fileName);
+    }
+
+
+
+
+
+
+    public void saveImage(String urls, String filename) throws IOException {
+        URL url = new URL(urls);
+        InputStream inputStream = url.openStream();
+        OutputStream outputStream = new FileOutputStream(filename);
+        byte[] b = new byte[2048];
+        int lenght;
+        while ((lenght = inputStream.read(b)) != - 1 ) {
+            outputStream.write(b, 0, lenght);
+        }
+        inputStream.close();
+        outputStream.close();
+
+
+    }
+
+    @Override
+    public String getBotToken(){
+        return "6605775103:AAHH2zLdMDOKimiBXkN2JHNKDcpbddh6tGc";
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "syncool_bot";
+    }
+}
